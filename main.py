64b5a5d20fd0d01dd6815505060c41712685246f
Bamdad <b.2113414@gmail.com>
2020-07-28 15:01:09 +0430

    added dictionary and simple word fetching functionality

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-# initializing project
+import random
+import math
+
+random.seed()
+randomly_chosen_words = []
+
+
+def get_dic_size(file):
+    count = 0
+    for l in file:
+        count += 1
+    return count
+
+
+def find_word(rcw):
+    with open('word_database.txt') as f:
+        all_lines = f.readlines()
+        ran = math.floor(random.random()*get_dic_size(all_lines))
+        rcw.append(all_lines[ran])
+        return rcw
+
+
+find_word(randomly_chosen_words)
+find_word(randomly_chosen_words)
+find_word(randomly_chosen_words)
+print(randomly_chosen_words)
+
+
